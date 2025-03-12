32db180fe814c3b7d638f63f8d8576cc7d7be2b9
Blaze Automation <rajib@blazeautomation.com>
2025-03-12 14:37:44 +0530

    ngFor TrackBy directives

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  list = [1,2,3,4,5,6];
+  studentArr: any[] = [ 
+    { 
+      "id": 1, 
+      "name": "student1" 
+    }, 
+    { 
+      "id": 2,
+      "name": "student2" 
+    }, 
+    { 
+      "id": 3, 
+      "name": "student3"
+    },  
+    { 
+      "id": 4, 
+      "name": "student4" 
+    } 
+  ]; 
+ 
+   trackByData(index:number, studentArr:any): number { 
+    return studentArr.id; 
+  } 
   constructor() { }
 
   ngOnInit(): void {
