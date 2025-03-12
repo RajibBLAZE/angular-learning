bf1cb29bcf25f85dcb523bd93582fac22138f2e5
Blaze Automation <rajib@blazeautomation.com>
2025-03-12 15:20:29 +0530

    Ngtemplate template

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,33 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user';
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: ` 
+    <div> 
+    <ng-template ngFor let-item [ngForOf]="Fruits" let-i="index"> 
+    <p>{{i}}</p> 
+    </ng-template> 
+    </div>`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  studentArr: any[] = [ 
-    { 
-      "id": 1, 
-      "name": "student1" 
-    }, 
-    { 
-      "id": 2,
-      "name": "student2" 
-    }, 
-    { 
-      "id": 3, 
-      "name": "student3"
-    },  
-    { 
-      "id": 4, 
-      "name": "student4" 
-    } 
-  ]; 
- 
-   trackByData(index:number, studentArr:any): number { 
-    return studentArr.id; 
-  } 
+  Fruits = ["mango","apple","orange","grapes"];
   constructor() { }
 
   ngOnInit(): void {
