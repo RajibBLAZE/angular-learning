fd474de58975e35c5532f3fa53d167598306e335
Blaze Automation <rajib@blazeautomation.com>
2025-03-12 13:19:09 +0530

    ngIfElse directives

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
+  isLogIn : boolean = false; 
+  isLogOut : boolean = true;
   constructor() { }
 
   ngOnInit(): void {
