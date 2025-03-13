a2d32d4a6a282e3ae092d62be9c3e47e5a4b1b91
Blaze Automation <rajib@blazeautomation.com>
2025-03-13 10:19:05 +0530

    template form

---
+++ src/app/prac/prac.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prac',
+  templateUrl: './prac.component.html',
+  styleUrls: ['./prac.component.css']
+})
+export class PracComponent implements OnInit {
+  username: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  OnClickedSubmit(name: string){
+    console.log("you are :" + name);
+  }
+}
