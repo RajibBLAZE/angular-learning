b4c1610f14c8be7377769fa0102b3000f9911688
Blaze Automation <rajib@blazeautomation.com>
2025-03-13 11:54:04 +0530

    Validators and FormBuilder

--- src/app/prac/prac.component.ts
+++ src/app/prac/prac.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms';
+import { FormControl,FormGroup,Validator,FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-prac',
   templateUrl: './prac.component.html',
   styleUrls: ['./prac.component.css']
 })
 export class PracComponent implements OnInit {
-  userName: string ='';
-  formdata: any;
-  constructor() { }
-
-  ngOnInit(): void {
-    this.formdata = new FormGroup({
-      userName: new FormControl("RAJIB")
+  //Create a FormGroup
+  requiredForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.myForm();
+   }
+  //create required field validator for name
+  myForm(){
+    this.requiredForm = this.fb.group({
+      name: ['', Validators.required]
     });
   }
-  OnClickedSubmit(data: any){
-    console.log(data)
-    this.userName = data.userName;
+  ngOnInit(): void {
+    
   }
+  
 }
