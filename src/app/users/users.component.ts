fc90367d8c1f20a10034b8273ce3947ba7e66d7d
Blaze Automation <rajib@blazeautomation.com>
2025-03-14 12:17:22 +0530

    Passing Query parameters to url route

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
   user: { id: string; name: string } = { id: '', name: '' };
 
   ngOnInit(): void {
     //Get Initial Route Parameters
     this.user = {
       id: this.route.snapshot.params['id'],
-      name: this.route.snapshot.params['name']
+      name: this.route.snapshot.params['name'],
     };
     // listening to route Parameter changes
-    this.route.params.subscribe((data: Params )=> {
+    this.route.params.subscribe((data: Params) => {
       this.user = {
         id: data['id'],
         name: data['name'],
-      }
+      };
     });
   }
-  
-
+  getRamaDetails() {
+    this.router.navigate(['/users', 2, 'Rama'], {
+      queryParams: { pages: 1, search: 'leela' },
+    });
+  }
 }
