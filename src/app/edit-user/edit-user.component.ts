1ab4617d6ddc6859ad8bb5dbeac75809ccc23c7c
Blaze Automation <rajib@blazeautomation.com>
2025-03-17 11:38:18 +0530

    CanDeactivate in real-time scenario

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { IDeactivateGuard } from './../services/guards/deactivate-guard.service';
+import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css']
+  styleUrls: ['./edit-user.component.css'],
 })
-export class EditUserComponent implements OnInit, IDeactivateGuard  {
-
-  constructor() { }
+export class EditUserComponent implements OnInit, IDeactivateGuard {
+  user: { id: string; name: string } = { id: '', name: '' };
+  editUserDetails: { id: string; name: string } = { id: '', name: '' };
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.user = {
+        id: data['id'],
+        name: data['name'],
+      };
+      this.editUserDetails = { ...this.user };
+    });
   }
-  canExit(){
-    if (confirm('Are you sure you want to exit')){
-      return true;
+  canExit(): boolean {
+    console.log(this.user);
+    console.log(this.editUserDetails);
+    if (
+      this.editUserDetails.id !== this.user.id ||
+      this.editUserDetails.name !== this.user.name
+    ){
+      if (confirm('All changes will be lost if you change the page')) {
+        return true;
+      }
+      else{
+        return false;
+      }
     }
+      
     return false;
   }
-
-
 }
